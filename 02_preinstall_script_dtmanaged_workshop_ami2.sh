334e1e867446cb4ba07c2f0763676454e8eda1a4
Gabriel B Casella <gbc921@gmail.com>
2020-11-05 01:15:46 -0300

    Add commands

--- 02_preinstall_script_dtmanaged_workshop_ami2.sh
+++ 02_preinstall_script_dtmanaged_workshop_ami2.sh
@@ -8,7 +8,10 @@
 # Create a user to give installation error
 sudo adduser -u 1011 conflict_user
 
-# Create the directories for Dynatrace Installation (not needed)
+# Create the directories for Dynatrace Installation (error)
 sudo mkdir -p /var/opt/dynatrace-managed/{agents,cassandra,elasticsearch,installer,server,server/replayData}
+
+# mount tmpfs to give not enough space error
+sudo mount -t tmpfs tmpfs /var/opt/dynatrace-managed/
 
 # vim:set ts=2 sw=2 et:
--- 03_undo_script_dtmanaged_workshop_ami2.sh
+++ 03_undo_script_dtmanaged_workshop_ami2.sh
@@ -5,12 +5,14 @@
 #
 ########################################################################
 
+# Umount tmpfs
+sudo umount /var/opt/dynatrace-managed/
+
+# Remove the directories for Dynatrace Installation (not needed)
+sudo rm -Rf /var/opt/dynatrace-managed
+
 # Remove user to allow installation
 sudo userdel -Z -r -f conflict_user
 sudo groupdel conflict_user
-
-
-# Remove the directories for Dynatrace Installation (not needed)
-sudo rm -R /var/opt/dynatrace-managed
 
 # vim:set ts=2 sw=2 et:
